a2460520aa0ffd697c97b439dd7ae338134d65be
Dennis Vriend <dennis.vriend@rabobank.nl>
2024-08-17 20:52:35 +0200

    updated front page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
+import "./App.css";
+import Projects from "../projects.ts";
 
 function App() {
-
-  return (
-    <>
-      <li>
-        <a href="https://Dennis539.github.io/SpaceInvaders">Space invaders</a><br>Simple game resembling the original space invaders game build in Typescript</br>
-      </li>
-      <li>
-        <a href="https://dennis539.github.io/PacmanAlgo/">Pacman clone</a><br>Simple game resembling the original space invaders game build in Typescript</br>
-      </li>
-    </>
-  )
+    return (
+        <>
+            <header>
+                <h1>Dennis Vriend's Github Pages</h1>
+            </header>
+            <li>
+                <a href="https://Dennis539.github.io/SpaceInvaders">
+                    Space invaders
+                </a>
+                {/* <br>Simple game resembling the original space invaders game build in Typescript</br> */}
+            </li>
+            <li>
+                <a href="https://dennis539.github.io/PacmanAlgo/">
+                    Pacman clone
+                </a>
+            </li>
+        </>
+    );
 }
 
-export default App
+export default App;
