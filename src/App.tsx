0485232c04d9716897a502bc769b5564c09eabd9
Dennis Vriend <dennis.vriend@rabobank.nl>
2024-08-18 15:44:29 +0200

    Reran bulld

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 // import { useState } from 'react'
 import styles from "./app.module.css";
-import Projects from "../projects.ts";
-import ProjectComponent from "./components/projectComponent.tsx";
+// import Projects from "../projects.ts";
+// import ProjectComponent from "./components/projectComponent.tsx";
 
 function App() {
-    const projectStruct = Projects.map((project, index) => (
-        <ProjectComponent
-            key={index}
-            name={project.Name}
-            description={project.Description}
-        />
-    ));
+    // const projectStruct = Projects.map((project, index) => (
+    //     <ProjectComponent
+    //         key={index}
+    //         name={project.Name}
+    //         description={project.Description}
+    //     />
+    // ));
     return (
         <>
             <header>
                 <h1>Dennis Vriend's Github Pages</h1>
             </header>
-            <div className={styles.projectsGrid}>{projectStruct}</div>
+            <div>
+                <h1>Hello world</h1>
+            </div>
+            {/* <div className={styles.projectsGrid}>{projectStruct}</div> */}
         </>
     );
 }
